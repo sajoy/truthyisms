c59898d7dc5c4668a3887c765bac60365d701fc4
Sarah Joy Calpo <sajoy.pc@gmail.com>
2018-08-16 11:37:17 -0700

    add fixed div to body

--- red.js
+++ red.js
@@ -1,7 +1,25 @@
 let p = document.querySelectorAll('p');
 p.forEach(paragraph => {
-    paragraph.style.color = 'red';
+    paragraph.style.color = 'green';
 })
 
 let b = document.body;
-b.style.border = '5px solid black'; 
+let h = b.scrollHeight;
+
+
+if (isEmptyWall()) {
+
+    let plaque = document.createElement('div');
+    plaque.classList.add('marble-bench');
+    plaque.innerText = 'you will never feel like it';
+
+    b.appendChild(plaque);
+    
+}
+    
+function isEmptyWall () {
+    let bodyStyles = window.getComputedStyle(document.body);
+    let bgStyle = bodyStyles.getPropertyValue('background');
+
+    return !bgStyle.includes('url');
+}

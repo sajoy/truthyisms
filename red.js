ae527a62a0075c47897716db37bd5f8c0f06a83e
Sarah Joy Calpo <sajoy.pc@gmail.com>
2018-08-21 14:05:48 -0700

    use Truism class to create and append marble bench

--- red.js
+++ red.js
@@ -1,27 +1,48 @@
-let p = document.querySelectorAll('p');
-p.forEach(paragraph => {
-    paragraph.style.color = 'green';
-})
-
-let b = document.body;
-let h = b.scrollHeight;
-
-
-if (isEmptyWall()) {
-
-    let plaque = document.createElement('div');
-    plaque.classList.add('marble-bench');
-    let p = document.createElement('p');
-    p.innerText = 'you will never feel like it';
+class Truism {
+    constructor () {
+        // TODO pull random truism from text file
+        let truism = 'you will never feel like it';
+        
+        this.setupElements(truism);
+    }
     
-    let url = chrome.extension.getURL('imgs/blackandwhite_small.jpg');
-    plaque.style.backgroundImage = `url(${url})`;
+    setupElements (truism) {
+        this.ele = document.createElement('div');
+        this.textEle = document.createElement('p');
+        
+        this.ele.title = 'Double click to dismiss this truism.';
+        this.textEle.innerText = truism;
 
-    plaque.appendChild(p);
-    b.appendChild(plaque);
+        this.ele.addEventListener('dblclick', e => {
+            document.body.removeChild(this.ele);
+        });    
 
+        this.ele.appendChild(this.textEle);
+    }
+
+    addToDom () {        
+        document.body.prepend(this.ele);
+    }
+    
+    styleMarble () {
+        this.ele.classList.add('marble-bench');
+        
+        let url = chrome.extension.getURL('imgs/blackandwhite_small.jpg');
+        this.ele.style.backgroundImage = `url(${url})`;
+    }
 }
+
+
+// TODO randomize when to add a truism (~30% of the time?)
+if (isEmptyWall()) {
+    let truism = new Truism();
     
+    // TODO randomize styling
+    truism.styleMarble();
+
+    truism.addToDom();
+}
+
 function isEmptyWall () {
     let bodyStyles = window.getComputedStyle(document.body);
     let bgStyle = bodyStyles.getPropertyValue('background');
